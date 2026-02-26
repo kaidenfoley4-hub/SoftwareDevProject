8367de40cc235444e039ec4185aeace6912b5767
Kaiden Foley <kaidenfoley4@gmail.com>
2026-02-26 18:06:26 +0000

    The Javascript isn't working trying something
    It is meant to swap the active form-box

---
+++ Software Development Project Ideas/Login.js
@@ -0,0 +1,7 @@
+function showFrom(formId){
+    document.getElementById("form-box").forEach(form => form.className-="active");
+    document.getElementById(formId).className+=" active";
+}
+const formBox=document.getElementById("form-box")
+const
+formBox.addEventListener()
