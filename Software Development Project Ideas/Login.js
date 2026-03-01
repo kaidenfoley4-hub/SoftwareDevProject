a18bc1fc97e5d7266d0dc3398bb1bb7441a635b1
Kaiden Foley <kaidenfoley4@gmail.com>
2026-03-01 12:31:13 +0000

    Added a checkbox for making the text for password visible
    The styling needs to be fixed

--- Software Development Project Ideas/Login.js
+++ Software Development Project Ideas/Login.js
@@ -1,3 +1,12 @@
 function showFrom(){
     document.querySelectorAll(".form-box").forEach(form => form.classList.toggle("active"));
 }
+
+function showPassword(){
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
